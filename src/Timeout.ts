349ecb129b3a0753874f4d5553ba7ef83ca0c15b
Sérgio Alencar <sergiofalencar@gmail.com>
2026-01-17 16:29:55 -0300

    commit inicial

---
+++ src/Timeout.ts
@@ -0,0 +1,31 @@
+// src/Timeout.ts
+
+export default class Timeout {
+  id;
+  handler;
+  start;
+  timeLeft;
+
+  constructor(handler: TimerHandler, time: number) {
+    this.id = setTimeout(handler, time);
+    this.handler = handler;
+    this.start = Date.now();
+    this.timeLeft = time;
+  }
+
+  clear() {
+    clearTimeout(this.id);
+  }
+
+  pause() {
+    const passed = Date.now() - this.start;
+    this.timeLeft -= passed;
+    this.clear();
+  }
+
+  continue() {
+    this.clear();
+    this.id = setTimeout(this.handler, this.timeLeft);
+    this.start = Date.now();
+  }
+}
